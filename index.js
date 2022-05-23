bdd8dbdd0deb7e11c34c42160a4aa479772df021
Mehraz Hossain <mehrazhossain54@gmail.com>
2022-05-24 02:52:21 +0600

    created get method for load users

--- index.js
+++ index.js
@@ -20,10 +20,32 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+// Middletier
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: 'UnAuthorized access' });
+  }
+  const token = authHeader.split(' ')[1];
+  // verify a token symmetric
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: 'Forbidden access' });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
 async function run() {
   try {
     await client.connect();
     const userCollection = client.db('blinkmotors-db').collection('users');
+
+    app.get('/user', async (req, res) => {
+      const users = await userCollection.find().toArray();
+      res.send(users);
+    });
 
     app.put('/user/:email', async (req, res) => {
       const email = req.params.email;
