34877e0bb003b0103a9f5e94b23d03ff1ff1aa73
Mehraz Hossain <mehrazhossain54@gmail.com>
2022-05-24 00:54:29 +0600

    create put method for saving new user into database

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 
@@ -9,6 +10,38 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// database
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yccrv.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const userCollection = client.db('blinkmotors-db').collection('users');
+
+    app.put('/user/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = userCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
+    });
+  } finally {
+    //
+  }
+}
+run().catch(console.dir);
+
+// Initial response for call root directory
 app.get('/', (req, res) => {
   res.send(`Server running on port ${port}`);
 });
